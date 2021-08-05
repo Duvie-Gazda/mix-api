3b5925a216300cc3a1acd28e9e4bac06af29f4aa
vitalii-pokrivchak <pokrivchakzvitalja@gmail.com>
2021-08-05 13:29:34 +0300

    init

---
+++ src/main/java/com/mix/api/MixApiApplication.java
@@ -0,0 +1,13 @@
+package com.mix.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MixApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MixApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mix/api/MixApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mix.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MixApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
