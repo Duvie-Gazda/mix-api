07e7e40d622d235d2e8a2c837a8202f83e50b890
vitalii-pokrivchak <pokrivchakzvitalja@gmail.com>
2021-08-06 21:33:56 +0300

    issue #5 solved

--- src/main/java/com/mix/api/security/UserDetailsService.java
+++ src/main/java/com/mix/api/security/UserDetailsService.java
@@ -1,13 +1,20 @@
 package com.mix.api.security;
 
+import com.mix.api.model.User;
+import com.mix.api.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
 
 @Component
 public class UserDetailsService implements org.springframework.security.core.userdetails.UserDetailsService {
 
+    @Autowired
+    private UserRepository userRepository;
+
     @Override
     public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
-        return null;
+        User user = userRepository.findByNick(s);
+        return UserDetails.fromUserToUserDetails(user);
     }
 }
