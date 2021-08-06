98fa3404f95258e4a7a8d51119ca61237edef801
Duvi-Gazda <gazda.haimdavid@gmail.com>
2021-08-06 16:34:52 +0300

    issue jarvis-technologies#12 solved

--- src/main/java/com/mix/api/repository/GroupRepository.java
+++ src/main/java/com/mix/api/repository/GroupRepository.java
@@ -2,6 +2,8 @@ package com.mix.api.repository;
 
 import com.mix.api.model.Group;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface GroupRepository extends JpaRepository<Group, Long> {
 }
--- src/main/java/com/mix/api/repository/UserRepository.java
+++ src/main/java/com/mix/api/repository/UserRepository.java
@@ -2,6 +2,8 @@ package com.mix.api.repository;
 
 import com.mix.api.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 }
